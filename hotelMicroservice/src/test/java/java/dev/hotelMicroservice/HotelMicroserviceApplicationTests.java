b6893c174023087f28a41bcce513ec7acc522aea
YexinioAguero <yexinio.aguero@ucr.ac.cr>
2024-04-19 21:25:10 -0600

    Se agrego el proyecto base

---
+++ hotelMicroservice/src/test/java/java/dev/hotelMicroservice/HotelMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package java.dev.hotelMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
